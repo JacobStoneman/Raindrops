ca8660d7b6cf2990c371719e8789780554ff41bc
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2021-02-09 10:56:12 +0000

    Added more test cases

--- Raindrops/RaindropsLib/Raindrops.cs
+++ Raindrops/RaindropsLib/Raindrops.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RaindropsLib
+﻿namespace RaindropsLib
 {
 	public class Raindrops
 	{
--- Raindrops/RaindropsTests/RaindropOutputTests.cs
+++ Raindrops/RaindropsTests/RaindropOutputTests.cs
@@ -7,9 +7,14 @@ namespace RaindropsTests
 	{
 		Raindrops Raindrops = new Raindrops(); 
 
+		//Test cases check for different configurations of results and edge cases
 		[TestCase (28, "Plong")]
+		[TestCase (-28, "Plong")]
 		[TestCase (30, "PlingPlang")]
+		[TestCase (21, "PlingPlong")]
+		[TestCase (105, "PlingPlangPlong")]
 		[TestCase (34, "34")]
+		[TestCase (-34, "-34")]
 		public void RaindropsReturnsCorrectOutput(int input, string expected)
 		{
 			Assert.That(Raindrops.GetDrops(input), Is.EqualTo(expected));
